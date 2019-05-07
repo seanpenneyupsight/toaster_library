c5e5c483d8f8c4ed36863599ae297619c5e41286
sean penney <seanjpenney@Gmail.com>
2019-05-07 15:31:41 -0700

    initial commit

---
+++ toasterlibrary/src/main/java/com/upsight/android/toasterlibrary/ToasterMessage.java
@@ -0,0 +1,19 @@
+package com.upsight.android.toasterlibrary;
+
+import android.content.Context;
+import android.os.Handler;
+import android.os.Looper;
+import android.widget.Toast;
+
+public class ToasterMessage {
+    public static void toastMethod(final Context c) {
+        //Toast.makeText(c, "Sean Hello World", Toast.LENGTH_SHORT).show();
+
+        new Handler(Looper.getMainLooper()).post(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(c, "Sean Hello World", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
